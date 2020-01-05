8fcb754316ea27836a68c261cdbbc16f26942a9a
mido <mydo.mughrabi@gmail.com>
2020-01-05 16:26:43 +0100

    evaluating binary classes

--- src/modeling/evaluator.py
+++ src/modeling/evaluator.py
@@ -7,13 +7,16 @@ def evaluate(true_data, predicted_data):
     #notice that both functions take the same parameters 
 
     #TODO: might need to use lables parameter
-    for av in [None ,'micro', 'macro', 'weighted']:
-        f1 = f1_score(true_data,predicted_data, average=av)
-        g_mean = geometric_mean_score(true_data, predicted_data, average=av)
-        result.update({"f_measure_" + str(av) : f1, "g_mean_" + str(av): g_mean})
+    for av in [None ,'micro', 'macro', 'weighted', 'binary']:
+        try:
+            f1 = f1_score(true_data,predicted_data, average=av)
+            g_mean = geometric_mean_score(true_data, predicted_data, average=av)
+            result.update({"f_measure_" + str(av) : f1, "g_mean_" + str(av): g_mean})
+        except:
+            pass
     return result
 
-y_true = [0, 1, 0, 0, 1, 1, 2]
-y_pred = [0, 0, 1, 0, 0, 1, 3]
+y_true = [0, 1, 0, 0, 1, 1]
+y_pred = [0, 0, 1, 0, 0, 1]
 print(evaluate(y_true, y_pred))
 
