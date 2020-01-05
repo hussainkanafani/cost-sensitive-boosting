8b7c4aad603cf5b6968cc4d28af09285decda1d7
mido <mydo.mughrabi@gmail.com>
2020-01-05 16:32:18 +0100

    make visualiser callable from the outside

--- src/modeling/visualiser.py
+++ src/modeling/visualiser.py
@@ -11,7 +11,8 @@ def plot_cost_fmeasue_gmean(costs, fmeasues, gmean):
     plt.legend(loc="upper left")
     plt.xlabel('Cost Setup')
     plt.ylabel('Metric Value')
-    plt.show()
+    #plt.show()
+    return plt
 
 def plot_instances_classes_weights_in_iteration(instances, classes, weights):
     COLOR_MAP = 'RdBu'
@@ -26,7 +27,7 @@ def plot_instances_classes_weights_in_iteration(instances, classes, weights):
     plt.colorbar(plt.cm.ScalarMappable(cmap=cmap))
     plt.xlabel('PCA 1')
     plt.ylabel('PCA 2')
-    plt.show()
+    #plt.show()
+    return plt
 
-#plot_instances_classes_weights_in_iteration([[1,2,3,4,5],[5,4,3,2,1],[1,2,4,4,5],[5,3,3,2,1]], [1,2,3,4], [0.1,0.2,0.7,0.9])
-plot_instances_classes_weights_in_iteration(np.random.randint(0,5,(10,5)),np.random.randint(0,6,10) , np.linspace(0,1,10))
+#plot_instances_classes_weights_in_iteration(np.random.randint(0,5,(10,5)),np.random.randint(0,6,10) , np.linspace(0,1,10))
