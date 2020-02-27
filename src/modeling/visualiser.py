8c77c6ea3f38cdd5455e33c0fc35b1f76ea051d2
aoomar33 <aoomar33@hotmail.com>
2020-02-27 08:53:49 +0100

    adding comments

--- src/modeling/visualiser.py
+++ src/modeling/visualiser.py
@@ -1,17 +1,27 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.decomposition import PCA
-import matplotlib.markers
 import numpy as np
 import io
 from PIL import Image
 
-def plot_cost_fmeasure_precision_recall(algorithm,costs, fmeasures,precision,recall):
+def plot_cost_fmeasure_precision_recall(algorithm, costs, fmeasures, precisions, recalls):
+    """
+
+    plots fmeasures, precisions, and recalls regarding the costs
+    
+    algorithm: name of the algorithm as string
+    costs: list of used costs to compute the metrics
+    fmeasures: list of computed fmeasures. The length equals len(costs)
+    precisions: list of computed precisions. The length equals len(costs)
+    recalls: list of computed recalls. The length equals len(costs)
+
+    """
+
     plt.clf()
     sns.set()
     plt.plot(costs, fmeasures, '--bo', label='F-measure')
-    plt.plot(costs, precision, '--go', label='Precision')
-    plt.plot(costs, recall, '--ro', label='Recall')
+    plt.plot(costs, precisions, '--go', label='Precision')
+    plt.plot(costs, recalls, '--ro', label='Recall')
     plt.legend(loc="upper left")
     plt.ylim(0,1)
     plt.xlabel('Cost Setup')    
@@ -20,6 +30,17 @@ def plot_cost_fmeasure_precision_recall(algorithm,costs, fmeasures,precision,rec
     return convert_plot_to_object(plt)
 
 def plot_fmeasure_imbalance_ratios(algorithm, ratios_setup, ratios_cost_setup, fmeasures):
+    """
+
+    plots the fmeasure of different costs regarding different imbalance ratios
+
+    algorithm: name of the algorithm as string
+    ratios_setup: list of ratios used to compute the fmeasures
+    ratios_cost_setup: list of costs used to compute the fmeaures
+    fmeasures: numpy array of shape (ratios_cost_setup, ratios_setup)
+
+    """
+
     plt.clf()
     sns.set()
 
@@ -33,24 +54,17 @@ def plot_fmeasure_imbalance_ratios(algorithm, ratios_setup, ratios_cost_setup, f
     plt.title(algorithm)
     return convert_plot_to_object(plt)
 
-def plot_instances_classes_weights_in_iteration(instances, classes, weights):
-    plt.clf()
-    COLOR_MAP = 'RdBu'
-    sns.set()
-    instancesAfterPca = PCA(n_components=2).fit_transform(instances)
-    markers = ['o','*','x','D','p','s']
-    cmap = matplotlib.cm.get_cmap(COLOR_MAP)
-    #plot type according to classes, color according to weight
-    for i in range(len(instancesAfterPca)):
-        plt.plot(instancesAfterPca[i,0], instancesAfterPca[i,1], marker=markers[int(classes[i])], c=cmap(weights[i]))
-    #show legend
-    plt.colorbar(plt.cm.ScalarMappable(cmap=cmap))
-    plt.xlabel('PCA 1')
-    plt.ylabel('PCA 2')
-    #plt.show()
-    return convert_plot_to_object(plt)
-
 def plot_stacked_barchart_weights_iterations(minority_weight_sums, majority_weight_sums, algorithm, cost):
+    """
+
+    plots a stacked barchart to show the behavior of an algorithm regarding its weight sum over the iterations
+
+    minority_weight_sums: list of weights sums of the minority class, the length equals number of iterations
+    majority_weight_sums: list of weights sums of the majority class, the length equals number of iterations
+    algorithm: name of the algorithm as string
+    cost: the used cost as string
+
+    """
     plt.clf()
     ind = np.arange(len(minority_weight_sums))
     width = 0.35
@@ -66,12 +80,10 @@ def plot_stacked_barchart_weights_iterations(minority_weight_sums, majority_weig
     return convert_plot_to_object(plt)
 
 def convert_plot_to_object(plt):
+    """ converts matplotlib object into a PIL image object """
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
     buf.seek(0)
     im = Image.open(buf)
-    #buf.close()
 
     return im
-#plot_instances_classes_weights_in_iteration(np.random.randint(0,5,(10,5)),np.random.randint(0,6,10) , np.linspace(0,1,10))
-#plot_stacked_barchart_weights_iterations(np.array([0.1, 0.0002, 0.3]), np.array([0.005, 0.01, 0.2]), 'adac1')
